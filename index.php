771e2945f3021bb8fe26510a539b9e31aab6cfdb
verm333r <verme333r@gmail.com>
2022-05-20 21:25:39 +0800

    penambahan kalimat dan file baru

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Facebook</title>
 </head>
 <body>
-    <p>Halo Ega</p>
+    <p>facebook</p>
+    <p>facebook membantu Anda terhubung dan berbagi dengan orang-orang dalam kehidupan Anda.</p>
 </body>
 </html>
