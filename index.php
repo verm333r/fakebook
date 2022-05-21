df693d19b2c376e0428a11291f2a88b0a72c15dd
verm333r <verme333r@gmail.com>
2022-05-21 10:51:37 +0800

    menambahkan banyak perubahan

--- index.php
+++ index.php
@@ -4,10 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Facebook</title>
 </head>
 <body>
     <p>facebook</p>
     <p>facebook membantu Anda terhubung dan berbagi dengan orang-orang dalam kehidupan Anda.</p>
+    <input type="text" class="email" id="email" placeholder="Email atau Nomor Telepon">
+    <input type="text" class="sandi" id="sandi" placeholder="Kata Sandi">
+    <button>Masuk</button>
+
+    <div>
+        <a href="https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0">Lupa Kata Sandi?</a>
+    </div>
+
+    <div>
+        <a href="#">Buat Akun Baru</a>
+    </div>
+
+    <div><a href="https://www.facebook.com/pages/create/?ref_type=registration_form">Buat Halaman</a> untuk selebriti, merek, atau bisnis.</div>
 </body>
 </html>
